3e31fe1c0a8d351a64cfd09d3d85f675c78b5a8f
sebastianwojciechowski6 <sebastian.wojciechowski@microsoft.wsei.edu.pl>
2020-01-17 18:12:19 +0100

    Way better, Artists to Albums added

--- appLogicTests/FiltersTests.cs
+++ appLogicTests/FiltersTests.cs
@@ -13,7 +13,7 @@ public class FiltersTest
     public void TestListAddValues()
     {
         var allFilters = new Filters();
-        Album album = new Album("Sword of death", "Metal", "Death Metal", 1998, "Recorded during cold day day", "Germany", allFilters);
+        Album album = new Album("Sword of death", new Artist("Jacek Cygan", "Fajny gość","Jacek Cygan" , "Polska"), "Metal", "Death Metal", 1998, "Recorded during cold day day", allFilters);
 
         var expected = "Metal";
 
